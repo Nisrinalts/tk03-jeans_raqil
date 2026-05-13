96b82f6d406149801a1c43b791ca8cb4eddd8e7e
Rayhan <rayhanderya9@gmail.com>
2026-05-13 22:36:20 +0700

    add trigger merah

---
+++ TK04_Trigger_A_JEANSRAQIL/TK04_Trigger_9_A_JEANSRAQIL.sql
@@ -0,0 +1,22 @@
+SET search_path TO tiktaktuk;
+
+CREATE OR REPLACE FUNCTION validate_seat_delete()
+RETURNS TRIGGER AS $$ 
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM tiktaktuk.ticket t
+        JOIN tiktaktuk.has_relationship hr ON t.ticket_id = hr.ticket_id
+        JOIN tiktaktuk.seat s ON hr.seat_id = s.seat_id
+        WHERE s.seat_id = OLD.seat_id
+    ) THEN
+        RAISE EXCEPTION 'ERROR: Kursi % - Baris % - No.% Tidak dapat dihapus karenas udah terisi.', OLD.section, OLD.row_number, OLD.seat_number;
+    END IF;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_validate_seat_delete
+BEFORE DELETE ON seat
+FOR EACH ROW
+EXECUTE FUNCTION validate_seat_delete();
